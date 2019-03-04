fd41681a481ebed9f9f9b94e2917f880b812891d
GravityZL <41053054+GravityZL@users.noreply.github.com>
2019-03-04 14:47:34 +0100

    Update single-sbatch-up.sh

--- blueprint_example/single-sbatch-up.sh
+++ blueprint_example/single-sbatch-up.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 cd single-sbatch-job
-cfy blueprints upload -b single-sbatch-job blueprint.yaml
+cfy blueprints upload -b sample-job blueprint.yaml
 read -n 1 -s -p "Press any key to continue"
 echo ''
-cfy deployments create -b single-sbatch-job -i ../local-blueprint-inputs.yaml --skip-plugins-validation single-sbatch-job
+cfy deployments create -b sample-job -i ../local-blueprint-inputs.yaml --skip-plugins-validation sample-job
 read -n 1 -s -p "Press any key to continue"
 echo ''
-cfy executions start -d single-sbatch-job install
+cfy executions start -d sample-job install
 read -n 1 -s -p "Press any key to continue"
 echo ''
-cfy executions start -d single-sbatch-job run_jobs
+cfy executions start -d sample-job run_jobs
